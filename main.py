3689ef06dbf4bce63080ff53dc722724f9daf572
DanielFiallo12 <97771340+DanielFiallo12@users.noreply.github.com>
2023-03-28 19:51:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -65,3 +65,42 @@ def main():
         else:
             print('Hasta luego')
             break
+    def menu(self):
+        self.cargar_datos()
+        while True:
+            print("""\n------- Bienvenido a Rent A Game ------- \n
+            1. Inserción de un nuevo juego.
+            2. Búsqueda de juego por modelo.
+            3. Búsqueda de juego por título.
+            4. Alquiler de un juego.
+            5. Devolución de un juego.
+            6. Eliminacion de un juego.
+            7. Salir
+            """)
+
+            while True:
+                try:
+                    option = int(input('\nIngrese una opción: '))
+                except ValueError:
+                    print("Debes escribir un número.")
+                    continue
+                else:
+                    break
+
+            if option == 1:
+                self.insertar_juego()
+            elif option == 2:
+                self.buscar_por_modelo()   
+            elif option == 3:
+                self.buscar_por_titulo()         
+            elif option == 4:
+                self.alquilar_juego()
+            elif option == 5:
+                self.devolver_juego()
+            elif option == 6:
+                self.eliminar_juego()
+            elif option == 7:
+                self.guardar_datos()
+                break
+            else:
+                print('Opción inválida')
